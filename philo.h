884a0972741906e386337279b13dcdbb83a3b69b
Sam Klaps <sklaps@a1-r3-p6.s19.be>
2024-09-05 16:14:31 +0200

    a

---
+++ philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sklaps <sklaps@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/05 14:23:42 by sklaps            #+#    #+#             */
+/*   Updated: 2024/09/05 16:13:42 by sklaps           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+typedef struct philosopher_s
+{
+	int		time_since_meal;
+	char	current_action;
+	int		n_forks;
+
+#endif
